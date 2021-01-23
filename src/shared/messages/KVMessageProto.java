2f6f8ac3ab263138c5f5751c2f36b5b9f254300b
ArjunM98 <mittal.arjun6978@gmail.com>
2021-01-22 21:51:41 -0500

    Defined ProtoBuff message format for communication

---
+++ src/shared/messages/KVMessageProto.java
@@ -0,0 +1,66 @@
+package shared.messages;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+import shared.messages.ProtoKVMessage.KVProto;
+
+public class KVMessageProto implements KVMessage {
+
+    private final KVProto msg;
+
+    /**
+     * Constructs a KV message format using protobuf builder.
+     *
+     * @param status - status type of message associated with message types and for identifying errors.
+     * @param key    - key associated with this message.
+     * @param value  - value associated with this message.
+     */
+    public KVMessageProto(StatusType status, String key, String value, long id) {
+        msg = KVProto.newBuilder()
+                .setStatusMsg(status.ordinal())
+                .setKeyMsg(key)
+                .setValueMsg(value)
+                .setIdMsg(id)
+                .build();
+    }
+
+    /**
+     * Reads and parses a message from an input stream.
+     *
+     * @param in - Input Stream.
+     */
+    public KVMessageProto(InputStream in) throws IOException {
+        msg = KVProto.parseDelimitedFrom(in);
+    }
+
+    public StatusType getStatus() {
+        return StatusType.values()[msg.getStatusMsg()];
+    }
+
+    public String getKey() {
+        return msg.getKeyMsg();
+    }
+
+    public String getValue() {
+        return msg.getValueMsg();
+    }
+
+    /**
+     * Serializes message and writes it to output stream.
+     *
+     * @param out - Output Stream.
+     */
+    public void writeMessageTo(OutputStream out) throws IOException {
+        msg.writeDelimitedTo(out);
+    }
+
+    /**
+     * @return returns a human readable format of KVProto
+     */
+    public String getMessageString() {
+        return msg.toString();
+    }
+
+}
--- src/testing/AdditionalTest.java
+++ src/testing/AdditionalTest.java
@@ -4,12 +4,61 @@ import org.junit.Test;
 
 import junit.framework.TestCase;
 
+import shared.messages.KVMessageProto;
+import shared.messages.KVMessage;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 public class AdditionalTest extends TestCase {
-	
-	// TODO add your test cases, at least 3
-	
-	@Test
-	public void testStub() {
-		assertTrue(true);
-	}
+
+    /**
+     * Tests KVProto Message Format - Status
+     */
+    @Test
+    public void testKVProtoGetStatus() {
+        KVMessageProto msg = new KVMessageProto(KVMessage.StatusType.GET, "key", "value", 1);
+        assertEquals(msg.getStatus(), KVMessage.StatusType.GET);
+    }
+
+    /**
+     * Tests KVProto Message Format - Key
+     */
+    @Test
+    public void testKVProtoGetKey() {
+        KVMessageProto msg = new KVMessageProto(KVMessage.StatusType.GET, "key", "value", 1);
+        assertEquals(msg.getKey(), "key");
+    }
+
+    /**
+     * Tests KVProto Message Format - Value
+     */
+    @Test
+    public void testKVProtoGetValue() {
+        KVMessageProto msg = new KVMessageProto(KVMessage.StatusType.GET, "key", "value", 1);
+        assertEquals(msg.getValue(), "value");
+    }
+
+    /**
+     * Tests KVProto Marshalling/Unmarshalling - This is achieved by marshalling and recovering the original text
+     */
+    @Test
+    public void testKVProtoWriteParseStream() throws Exception {
+        KVMessageProto msgSend = new KVMessageProto(KVMessage.StatusType.GET, "key", "value", 1);
+        KVMessageProto msgRecv;
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            msgSend.writeMessageTo(out);
+
+            try (ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray())) {
+                msgRecv = new KVMessageProto(in);
+            }
+
+        }
+
+        assertEquals(msgSend.getKey(), msgRecv.getKey());
+        assertEquals(msgSend.getValue(), msgRecv.getValue());
+        assertEquals(msgSend.getStatus(), msgRecv.getStatus());
+    }
+
 }
