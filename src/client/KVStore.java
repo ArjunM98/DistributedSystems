14e9baec98e7037ff5cf2ef38f6f26a1aa7d7a7c
ArjunM98 <mittal.arjun6978@gmail.com>
2021-01-24 12:11:52 -0500

    Communication protocol for interacting between client and server in addition to KVStore library

--- src/client/KVStore.java
+++ src/client/KVStore.java
@@ -1,36 +1,129 @@
 package client;
 
 import shared.messages.KVMessage;
+import shared.messages.KVMessageProto;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class KVStore implements KVCommInterface {
-	/**
-	 * Initialize KVStore with address and port of KVServer
-	 * @param address the address of the KVServer
-	 * @param port the port of the KVServer
-	 */
-	public KVStore(String address, int port) {
-		// TODO Auto-generated method stub
-	}
 
-	@Override
-	public void connect() throws Exception {
-		// TODO Auto-generated method stub
-	}
+    private final String addr;
+    private final int portNum;
 
-	@Override
-	public void disconnect() {
-		// TODO Auto-generated method stub
-	}
+    private Socket clientSocket;
+    private OutputStream output;
+    private InputStream input;
+    private long msgID;
+    private boolean running;
 
-	@Override
-	public KVMessage put(String key, String value) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private static final int MAX_KEY_SIZE = 20;
+    private static final int MAX_VALUE_SIZE = 120 * 1024;
 
-	@Override
-	public KVMessage get(String key) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Initialize KVStore with address and port of KVServer
+     *
+     * @param address the address of the KVServer
+     * @param port    the port of the KVServer
+     */
+    public KVStore(String address, int port) {
+        addr = address;
+        portNum = port;
+    }
+
+    @Override
+    public void connect() throws Exception {
+        clientSocket = new Socket(addr, portNum);
+        input = clientSocket.getInputStream();
+        output = clientSocket.getOutputStream();
+        msgID = 0;
+        setRunning(true);
+    }
+
+    @Override
+    public void disconnect() {
+        try {
+            tearDownConnection();
+        } catch (IOException e) {
+            // TODO: replace with logger
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public KVMessage put(String key, String value) throws Exception {
+
+        incrementMsgID();
+
+        if (malformedKey(key) & malformedValue(value)) {
+            return new KVMessageProto(KVMessage.StatusType.FAILED, key, value, msgID);
+        }
+
+        try {
+            KVMessageProto req = new KVMessageProto(KVMessage.StatusType.PUT, key, value, msgID);
+            req.writeMessageTo(output);
+            return new KVMessageProto(input);
+        } catch (IOException e) {
+            if (isRunning()) {
+                disconnect();
+            }
+            throw e;
+        }
+    }
+
+    @Override
+    public KVMessage get(String key) throws Exception {
+
+        incrementMsgID();
+
+        if (malformedKey(key)) {
+            return new KVMessageProto(KVMessage.StatusType.FAILED, key, "" /* empty value */, msgID);
+        }
+
+        try {
+            KVMessageProto req = new KVMessageProto(KVMessage.StatusType.GET, key, "" /* empty value */, msgID);
+            req.writeMessageTo(output);
+            return new KVMessageProto(input);
+        } catch (IOException e) {
+            if (isRunning()) {
+                disconnect();
+            }
+            throw e;
+        }
+    }
+
+    private void setRunning(boolean run) {
+        running = run;
+    }
+
+    private boolean isRunning() {
+        return running;
+    }
+
+    private void tearDownConnection() throws IOException {
+        setRunning(false);
+        if (clientSocket != null) {
+            input.close();
+            output.close();
+            clientSocket.close();
+            clientSocket = null;
+        }
+    }
+
+    private boolean malformedKey(String key) {
+        if (key.length() == 0) {
+            return true;
+        } else return key.length() >= MAX_KEY_SIZE;
+    }
+
+    private boolean malformedValue(String value) {
+        return value.length() >= MAX_VALUE_SIZE;
+    }
+
+    private void incrementMsgID() {
+        msgID += 1;
+    }
+
 }
