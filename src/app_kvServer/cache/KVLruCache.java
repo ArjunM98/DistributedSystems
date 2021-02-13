8ad418c3f3207b258b9c7203b095afcafb04402f
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2021-02-13 14:08:12 -0500

    Add main methods (todo: replace with junit tests)

--- src/app_kvServer/cache/KVLruCache.java
+++ src/app_kvServer/cache/KVLruCache.java
@@ -84,4 +84,62 @@ public class KVLruCache implements IKVCache {
             lock.writeLock().unlock();
         }
     }
+
+    public static void main(String[] args) {
+        final int TEST_CACHE_SIZE = 100;
+        final IKVCache cache = new KVLruCache(TEST_CACHE_SIZE);
+
+        /* CACHE FUNCTIONALITY TEST*/
+
+        // Fill up the cache
+        for (int i = 0; i < TEST_CACHE_SIZE; i++) cache.putKV("Key_" + i, "Value_" + i);
+
+        // Ensure the cache is working as intended; reverse loop so FIFO and LRU difference are shown
+        for (int i = TEST_CACHE_SIZE - 1; i >= 0; i--) {
+            final String cacheValue = cache.getKV("Key_" + i), expectedValue = "Value_" + i;
+            if (!cacheValue.equals(expectedValue)) {
+                throw new AssertionError("Expected " + expectedValue + " / Got " + cacheValue);
+            }
+        }
+
+        /* SINGLE EVICTION TEST */
+
+        cache.putKV("Key_" + TEST_CACHE_SIZE, "Value_" + TEST_CACHE_SIZE);
+
+        // The new key should be there
+        if (cache.getCacheSize() == TEST_CACHE_SIZE) {
+            final String cacheValue = cache.getKV("Key_" + TEST_CACHE_SIZE), expectedValue = "Value_" + TEST_CACHE_SIZE;
+            if (!cacheValue.equals(expectedValue)) {
+                throw new AssertionError("Expected " + expectedValue + " / Got " + cacheValue);
+            }
+        } else throw new AssertionError("Expected " + TEST_CACHE_SIZE + " / Got " + cache.getCacheSize());
+
+        // The LRU key should not
+        if (cache.inCache("Key_" + (TEST_CACHE_SIZE - 1))) {
+            throw new AssertionError(String.format("Key_%s should have been deleted", (TEST_CACHE_SIZE - 1)));
+        }
+
+        /* FULL EVICTION TEST */
+
+        // Fill up the cache
+        for (int i = 0; i < TEST_CACHE_SIZE; i++) cache.putKV("New_Key_" + i, "New_Value_" + i);
+
+        // Ensure old keys are gone
+        for (int i = 0; i < TEST_CACHE_SIZE; i++) {
+            final String cacheValue = cache.getKV("Key_" + i);
+            if (cacheValue != null) {
+                throw new AssertionError("Expected null / Got " + cacheValue);
+            }
+        }
+
+        // And new keys are in
+        for (int i = 0; i < TEST_CACHE_SIZE; i++) {
+            final String cacheValue = cache.getKV("New_Key_" + i), expectedValue = "New_Value_" + i;
+            if (!cacheValue.equals(expectedValue)) {
+                throw new AssertionError("Expected " + expectedValue + " / Got " + cacheValue);
+            }
+        }
+
+        System.out.printf("%s is working%n", cache.getClass());
+    }
 }
