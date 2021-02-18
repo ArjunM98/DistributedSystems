88c99e93e5f4d8fd9aa3f2c2c41d7042077a1a9f
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2021-02-17 23:11:30 -0500

    Add M2 status types to enum - Also update Javadocs of KVCommInterface as shown in Quercus guide

--- src/client/KVCommInterface.java
+++ src/client/KVCommInterface.java
@@ -4,35 +4,37 @@ import shared.messages.KVMessage;
 
 public interface KVCommInterface {
     /**
-     * Establishes a connection to the KV Server.
+     * Establishes a connection to storage service, i.e., to an arbitrary
+     * instance of the storage servers that makes up the storage service.
      *
      * @throws Exception if connection could not be established.
      */
     public void connect() throws Exception;
 
     /**
-     * disconnects the client from the currently connected server.
+     * Disconnects the client from the storage service (i.e., the currently
+     * connected server).
      */
     public void disconnect();
 
     /**
-     * Inserts a key-value pair into the KVServer.
+     * Inserts a data record into the storage service.
      *
      * @param key   the key that identifies the given value.
      * @param value the value that is indexed by the given key.
      * @return a message that confirms the insertion of the tuple or an error.
-     * @throws Exception if put command cannot be executed (e.g. not connected to any
-     *                   KV server).
+     * @throws Exception if put command cannot be executed
+     *                   (e.g. not connected to any storage server).
      */
     public KVMessage put(String key, String value) throws Exception;
 
     /**
-     * Retrieves the value for a given key from the KVServer.
+     * Retrieves a data record for a given key from the storage service.
      *
-     * @param key the key that identifies the value.
+     * @param key the key that identifies the record.
      * @return the value, which is indexed by the given key.
-     * @throws Exception if put command cannot be executed (e.g. not connected to any
-     *                   KV server).
+     * @throws Exception if get command cannot be executed
+     *                   (e.g. not connected to any storage server).
      */
     public KVMessage get(String key) throws Exception;
 }
