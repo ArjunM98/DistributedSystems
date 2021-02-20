59f27d1da9d3dfc53e449348778fba3a9fdf8b47
Amar <32231895+arefeena@users.noreply.github.com>
2021-02-19 22:33:03 -0500

    Consistent hashing (#38)
    * Initial hash ring / metadata implementation
    
    * Hash ring serialization implementation
    
    * Update javadoc for hashRing

---
+++ src/ecs/ECSHashRing.java
@@ -0,0 +1,132 @@
+package ecs;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class ECSHashRing {
+    /**
+     * A {@link java.util.NavigableMap} representing the hash ring of servers
+     */
+    private final TreeMap<BigInteger, ECSNode> hashRing = new TreeMap<>();
+
+    /**
+     * Parse and construct an ECSHashRing according to the example ecs.config file provided on Quercus
+     *
+     * @param config string with lines like "server1 localhost 50000"
+     * @return constructed ECSHashRing
+     * @throws IllegalArgumentException if lines are poorly formatted
+     */
+    public static ECSHashRing fromConfig(String config) {
+        return new ECSHashRing(config.lines().map(ECSNode::fromConfig).toArray(ECSNode[]::new));
+    }
+
+    /**
+     * Serialize this ECSHashRing into the format provided in ecs.config
+     *
+     * @return string representation of this node
+     */
+    public String toConfig() {
+        return this.hashRing.values().stream().map(ECSNode::toConfig).collect(Collectors.joining("\n"));
+    }
+
+    /**
+     * Construct an ECSHashRing with zero or more given nodes
+     *
+     * @param nodes ECSNodes to include in the hash ring
+     */
+    public ECSHashRing(ECSNode... nodes) {
+        for (ECSNode node : nodes) this.addServer(node);
+    }
+
+    /**
+     * @param payload is the string to hash
+     * @return MD5 Hash of String in {@link BigInteger} format
+     */
+    public static BigInteger computeHash(String payload) {
+        try {
+            final MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(payload.getBytes());
+            return new BigInteger(1, md5.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("System does not support MD5 hashing");
+        }
+    }
+
+    /**
+     * Wrapper for {@link #getServer(BigInteger)}
+     *
+     * @param payload string holding key or ip:port
+     * @return the {@link ECSNode} this payload maps to
+     */
+    public ECSNode getServer(String payload) {
+        return getServer(computeHash(payload));
+    }
+
+    /**
+     * Get server responsible for the given hash, considering wraparound as defined in Quercus guidelines
+     *
+     * @param hash {@link BigInteger} representation of an MD5 hash
+     * @return the {@link ECSNode} responsible for this hash
+     */
+    public ECSNode getServer(BigInteger hash) {
+        if (hashRing.isEmpty()) return null;
+        if (hashRing.size() == 1) return hashRing.firstEntry().getValue();
+
+        if (hash.compareTo(hashRing.lastKey()) > 0) {
+            return hashRing.firstEntry().getValue();
+        } else {
+            return hashRing.ceilingEntry(hash).getValue();
+        }
+    }
+
+    /**
+     * Add a server to the hash ring. Update is done in-place.
+     *
+     * @param server to add to ring
+     */
+    public void addServer(ECSNode server) {
+        if (hashRing.isEmpty()) {
+            hashRing.put(server.getNodeHash(), server);
+        } else {
+            // Tell successor its no longer responsible for this hash
+            ECSNode successor = getServer(server.getNodeHash());
+            if (successor == null) successor = server;
+            successor.setPredecessor(server);
+
+            // Add to hash ring
+            hashRing.put(server.getNodeHash(), server);
+
+            // Tell this server its lower bound for hashes
+            final Map.Entry<BigInteger, ECSNode> lowerEntry = hashRing.lowerEntry(server.getNodeHash());
+            ECSNode predecessor = lowerEntry == null ? hashRing.lastEntry().getValue() : lowerEntry.getValue();
+            server.setPredecessor(predecessor);
+        }
+    }
+
+    /**
+     * Remove a server from the hash ring. Update is done in-place.
+     *
+     * @param server to remove from ring
+     */
+    public void removeServer(ECSNode server) {
+        if (hashRing.size() <= 1) {
+            hashRing.remove(server.getNodeHash());
+        } else {
+            // Get predecessor ready
+            final Map.Entry<BigInteger, ECSNode> lowerEntry = hashRing.lowerEntry(server.getNodeHash());
+            ECSNode predecessor = lowerEntry == null ? null : lowerEntry.getValue();
+
+            // Remove from hash ring
+            hashRing.remove(server.getNodeHash());
+
+            // Tell successor it's again responsible for this hash range
+            ECSNode successor = getServer(server.getNodeHash());
+            if (predecessor == null) predecessor = hashRing.lastEntry().getValue();
+            successor.setPredecessor(predecessor);
+        }
+    }
+}
---
+++ src/ecs/ECSNode.java
@@ -0,0 +1,135 @@
+package ecs;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
+
+public class ECSNode implements IECSNode {
+    private final String nodeName, nodeHost;
+    private final int nodePort;
+
+    private final BigInteger nodeHash;
+    private BigInteger predecessorHash;
+
+    /**
+     * Parse and construct an ECSNode according to the example ecs.config file provided on Quercus
+     *
+     * @param config string like "server1 localhost 50000"
+     * @return constructed ECSNode
+     * @throws IllegalArgumentException if line is poorly formatted
+     */
+    public static ECSNode fromConfig(String config) {
+        try {
+            final String DELIMITER = " ";
+            final List<String> tokens = Arrays.asList(config.split(DELIMITER));
+            if (tokens.size() < 3) throw new IllegalArgumentException("Expected 3 tokens");
+
+            return new ECSNode(
+                    String.join(DELIMITER, tokens.subList(0, tokens.size() - 2)),
+                    tokens.get(tokens.size() - 2),
+                    Integer.parseInt(tokens.get(tokens.size() - 1))
+            );
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unable to parse config from " + config, e);
+        }
+    }
+
+    /**
+     * Serialize this ECSNode into the format provided in ecs.config
+     *
+     * @return string representation of this node
+     */
+    public String toConfig() {
+        return this.nodeName + " " + this.nodeHost + " " + this.nodePort;
+    }
+
+    /**
+     * Construct an ECSNode given explicit construction values
+     *
+     * @param nodeName human-readable identifier for the node
+     * @param nodeHost IP/hostname of the node
+     * @param nodePort port on which the node listens for client connections
+     */
+    public ECSNode(String nodeName, String nodeHost, int nodePort) {
+        this.nodeName = nodeName;
+        this.nodeHost = nodeHost;
+        this.nodePort = nodePort;
+
+        this.nodeHash = ECSHashRing.computeHash(this.getConnectionString());
+        this.setPredecessor(this);
+    }
+
+    /**
+     * Save the hash of the predecessor of this node from the {@link ECSHashRing} to build this node's range
+     */
+    public void setPredecessor(ECSNode predecessor) {
+        this.predecessorHash = predecessor.getNodeHash();
+    }
+
+    /**
+     * @return the connection string that uniquely identifies this node
+     */
+    public String getConnectionString() {
+        return this.nodeHost + ":" + this.nodePort;
+    }
+
+    /**
+     * @return {@link #nodeHash}
+     */
+    public BigInteger getNodeHash() {
+        return this.nodeHash;
+    }
+
+    /**
+     * Checks if a key's hash is within this node's hash range
+     *
+     * @param key to hash and check against this node's range
+     * @return true if this ECS node is responsible for a given Key
+     */
+    public boolean isResponsibleForKey(String key) {
+        return this.isResponsibleForHash(ECSHashRing.computeHash(key));
+    }
+
+    /**
+     * Checks if an MD5 value is within this node's hash range
+     *
+     * @param hash to check against this node's range
+     * @return true if {@link #predecessorHash} < hash <= {@link #nodeHash}
+     */
+    private boolean isResponsibleForHash(BigInteger hash) {
+        return this.nodeHash.compareTo(hash) >= 0
+                && this.predecessorHash.compareTo(hash) < 0;
+    }
+
+    /**
+     * @return the name of the node (ie "Server 8.8.8.8")
+     */
+    @Override
+    public String getNodeName() {
+        return this.nodeName;
+    }
+
+    /**
+     * @return the hostname of the node (ie "8.8.8.8")
+     */
+    @Override
+    public String getNodeHost() {
+        return this.nodeHost;
+    }
+
+    /**
+     * @return the port number of the node (ie 8080)
+     */
+    @Override
+    public int getNodePort() {
+        return this.nodePort;
+    }
+
+    /**
+     * @return array of two strings representing the low and high range of the hashes that the given node is responsible for
+     */
+    @Override
+    public String[] getNodeHashRange() {
+        return new String[]{this.predecessorHash.toString(16), this.nodeHash.toString(16)};
+    }
+}
