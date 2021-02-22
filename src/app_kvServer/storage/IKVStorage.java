05068a6ca2a3b3a0539e33199bd879e4d9f36195
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2021-02-21 20:34:51 -0500

    Implement compaction and kvserver data transfer methods - Right now compaction is only run at the start of a data transfer to minimize network I/O   - Might be wise to also have a compaction daemon in KVSingleFileStorage that runs periodically? - Data transfer methods are lazily populated streams for efficiency   - The resultant stream can be used to send messages (e.g. across a socket) to another server, which can use that socket to create a lazily populated stream which it passes into its deserializer?   - Exact usage might change bc design choices, but for now I've included a barebones test in AdditionalTest.java - Also fix bug in PerformanceTests.java

--- src/app_kvServer/storage/IKVStorage.java
+++ src/app_kvServer/storage/IKVStorage.java
@@ -3,6 +3,9 @@ package app_kvServer.storage;
 import app_kvServer.KVServerException;
 import shared.messages.KVMessage.StatusType;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public interface IKVStorage {
     public static final String STORAGE_ROOT_DIRECTORY = "data";
 
@@ -40,4 +43,47 @@ public interface IKVStorage {
      * Clear the storage of the server
      */
     public void clearStorage();
+
+    /**
+     * Get a stream of all {@link KVPair}s in storage which match a certain criteria.
+     * <p>
+     * This stream is likely backed by a file handle so callers should Remember to call {@link Stream#close()} on the
+     * resulting stream after it's been processed, or use within a try-with-resources statement.
+     *
+     * @return {@link Stream} of {@link KVPair} (ideally lazily populated) or empty stream on error
+     */
+    public Stream<KVPair> openKvStream(Predicate<KVPair> filter);
+
+    /**
+     * Container class for a key-value pair
+     */
+    class KVPair {
+        /**
+         * According to M1 docs, keys will never have a space in them
+         */
+        public static final String KV_DELIMITER = " ";
+
+        public final String key, value;
+
+        public KVPair(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        /**
+         * @return serialized string for KVPair
+         */
+        public String serialize() {
+            return this.key + KV_DELIMITER + this.value;
+        }
+
+        /**
+         * @param serialized see {@link #serialize()}
+         * @return deserialized instance of {@link KVPair} or null on failure
+         */
+        public static KVPair deserialize(String serialized) {
+            final int split = serialized.indexOf(KV_DELIMITER);
+            return split < 0 ? null : new KVPair(serialized.substring(0, split), serialized.substring(split + 1));
+        }
+    }
 }
