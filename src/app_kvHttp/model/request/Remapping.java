a194dda564d682330bc74b999704b136087afa9e
Amar Arefeen <amar.arefeen@mail.utoronto.ca>
2021-04-08 18:12:09 -0400

    Update remapping to find/replace as discussed

--- src/app_kvHttp/model/request/Remapping.java
+++ src/app_kvHttp/model/request/Remapping.java
@@ -5,24 +5,37 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @SuppressWarnings("unused")
 public class Remapping extends Model {
-    private final String value;
+    private final Pattern find;
+    private final String replace;
 
     /**
      * JSON deserializer hook for Jackson
      */
     @JsonCreator
-    public Remapping(@JsonProperty(value = "value", required = true) String value) {
-        this.value = value;
+    public Remapping(
+            @JsonProperty(value = "find", required = true) String find,
+            @JsonProperty(value = "replace", required = true) String replace
+    ) {
+        this.find = Pattern.compile(find);
+        this.replace = replace;
     }
 
     /**
      * Getter required for JSON serialization
      */
-    public String getValue() {
-        return value;
+    public Pattern getFind() {
+        return find;
+    }
+
+    /**
+     * Getter required for JSON serialization
+     */
+    public String getReplace() {
+        return replace;
     }
 
     /**
@@ -30,7 +43,7 @@ public class Remapping extends Model {
      */
     public static void main(String[] args) throws Exception {
         // 1. Generate test case
-        final String json = Model.toString(Map.of("value", "new_value"));
+        final String json = Model.toString(Map.of("find", "needle", "replace", "new_value"));
         System.out.println("test = " + json);
 
         // 2. Deserialize from JSON
